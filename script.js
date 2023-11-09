6757fa7f78111eb007e48ef6b342b0ee4ffa8e1a
Koziah Sylvester <112087322+Koziah-Sylvester@users.noreply.github.com>
2023-11-09 01:01:12 +0000

    Remove and REset

--- script.js
+++ script.js
@@ -1,34 +1,64 @@
-/*!
-* Start Bootstrap - Resume v7.0.6 (https://startbootstrap.com/theme/resume)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
+import sqlite3InitModule from '@sqlite.org/sqlite-wasm';
 
-window.addEventListener('DOMContentLoaded', event => {
-
-    // Activate Bootstrap scrollspy on the main nav element
-    const sideNav = document.body.querySelector('#sideNav');
-    if (sideNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#sideNav',
-            rootMargin: '0px 0px -40%',
-        });
-    };
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
+console.log(w3);
+const logHtml = function (cssClass, ...args) {
+    postMessage({
+      type: 'log',
+      payload: { cssClass, args },
     });
-
-});
+  };
+  
+  const log = (...args) => logHtml('', ...args);
+  const error = (...args) => logHtml('error', ...args);
+  
+  const start = function (sqlite3) {
+    const capi = sqlite3.capi; // C-style API
+    const oo = sqlite3.oo1; // High-level OO API
+    log('SQLite3 version', capi.sqlite3_libversion(), capi.sqlite3_sourceid());
+    let db;
+    if ('OpfsDb' in oo) {
+      db = new oo.OpfsDb('/mydb.sqlite3');
+      log('The OPFS is available.');
+      log('Persisted db =', db.filename);
+    } else {
+      db = new oo.DB('/mydb.sqlite3', 'ct');
+      log('The OPFS is not available.');
+      log('transient db =', db.filename);
+    }
+  
+    try {
+      log('Create a table...');
+      db.exec('CREATE TABLE IF NOT EXISTS t(a,b)');
+      log('Insert some data using exec()...');
+      let i;
+      for (i = 20; i <= 25; ++i) {
+        db.exec({
+          sql: 'INSERT INTO t(a,b) VALUES (?,?)',
+          bind: [i, i * 2],
+        });
+      }
+      log("Query data with exec() using rowMode 'array'...");
+      db.exec({
+        sql: 'SELECT a FROM t ORDER BY a LIMIT 3',
+        rowMode: 'array', // 'array' (default), 'object', or 'stmt'
+        callback: function (row) {
+          log('row ', ++this.counter, '=', row);
+        }.bind({ counter: 0 }),
+      });
+    } finally {
+      db.close();
+    }
+  };
+  
+  log('Loading and initializing sqlite3 module...');
+  sqlite3InitModule({
+    print: log,
+    printErr: error,
+  }).then(function (sqlite3) {
+    log('Done initializing. Running demo...');
+    try {
+      start(sqlite3);
+    } catch (e) {
+      error('Exception:', e.message);
+    }
+  });
